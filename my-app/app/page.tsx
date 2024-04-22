6df92be0471c32d0232d0177503ee77c05d78a0f
ysahih <youssefsahih9@gmail.com>
2024-04-22 17:05:06 +0100

    add: navbar

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,4 +1,14 @@
+"use client"
+
 import Image from "next/image";
+import { CiHome } from "react-icons/ci";
+import { GoPerson } from "react-icons/go";
+import { GiSkills } from "react-icons/gi";
+import { GoProjectRoadmap } from "react-icons/go";
+import { CiHeadphones } from "react-icons/ci";
+import { useEffect } from "react";
+import WebFont from 'webfontloader';
+
 
 const Home = ()=> {
   return(
@@ -29,15 +39,38 @@ const Contact = ()=> {
     <section></section>
   );
 }
-
 const NavBar = () =>{
+  //loading fonts
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Droid Sans', 'Chilanka']
+      }
+    });
+   }, []);
+ 
   return (
-    <ul id="navbar">
-        <li><a className="" href="#welcome-section">Home</a></li>
-        <li><a className="" href="#about">About</a></li>
-        <li><a className="" href="#skills">Skills</a></li>
-        <li><a className="" href="#projects">Work</a></li>
-        <li><a className="" href="#contact">Contact</a></li>
+    <ul className="navbar flex fixed bottom-0 w-full">
+        <li><a className="section flex" href="#welcome-">
+            <CiHome className="Picon"/>  <span>Home</span></a>
+        </li>
+
+        <li><a className="section flex" href="#about">
+          <GoPerson className="Picon"/> <span>About</span></a>
+        </li>
+
+        <li><a className="section flex" href="#skills">
+          <GiSkills className="Picon"/> <span>Skills</span></a>
+        </li>
+
+        <li><a className="section flex" href="#projects">
+          <GoProjectRoadmap className="Picon"/> <span>Work</span></a>
+        </li>
+        
+        <li><a className="section flex" href="#contact">
+          <CiHeadphones className="Picon"/>  <span>Contact</span></a>
+        </li>
+        
     </ul>
   );
 }
