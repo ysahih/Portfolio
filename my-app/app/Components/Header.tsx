dff0cfd2034d1172428a35de53f64030c7b5e29c
ysahih <youssefsahih9@gmail.com>
2024-04-23 18:02:47 +0100

    add: resume button

--- my-app/app/Components/Header.tsx
+++ my-app/app/Components/Header.tsx
@@ -5,6 +5,8 @@ import { ImTwitter } from "react-icons/im";
 import { ImFacebook2 } from "react-icons/im";
 import { ImInstagram } from "react-icons/im";
 import { LiaPhoneAltSolid } from "react-icons/lia";
+import { IoCloudDownloadOutline } from "react-icons/io5";
+
 
 
 import Form from "./Form";
@@ -34,28 +36,39 @@ const Header = ()=>{
         </div>
         {contact && <div className="Connect">
             <div className="connecHead">
+                <hr className="line"/>
                 <h2>Connect with me!</h2>
             </div>
             {/* <hr></hr> */}
             <div className="infos">
-                <div className="socials">
-                    <div className="smContainer">
-                        <ImGithub    className="socialIcon"/>
+                <div className="sideIcons">
+
+                    <div className="socials">
+                        <div className="smContainer">
+                            <ImGithub    className="socialIcon"/>
+                        </div>
+                        <div className="smContainer">
+                            <ImLinkedin  className="socialIcon squared"/>
+                        </div>
+                        <div className="smContainer">
+                            <ImFacebook2 className="socialIcon squared" />
+                        </div>
+                        <div className="smContainer">
+                            <ImInstagram className="socialIcon squared" />
+                        </div>
+                        <div className="smContainer">
+                            <ImTwitter   className="socialIcon"/>
+                        </div>
                     </div>
-                    <div className="smContainer">
-                        <ImLinkedin  className="socialIcon squared"/>
-                    </div>
-                    <div className="smContainer">
-                        <ImFacebook2 className="socialIcon squared" />
-                    </div>
-                    <div className="smContainer">
-                        <ImInstagram className="socialIcon squared" />
-                    </div>
-                    <div className="smContainer">
-                        <ImTwitter   className="socialIcon"/>
+
+                    <div className="resume">
+                        <div className="resumeContainer">
+                            <IoCloudDownloadOutline />
+                            <p>RESUME</p>
+                        </div>
                     </div>
                 </div>
-
+                
                 <div className="">
                     <div className="myPhoto">
                         <Image 
